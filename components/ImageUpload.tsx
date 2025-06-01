9d515af994d5c0d824c2ca826116c9e14f3cb8b1
AbdullahSajid18 <abdullahsajid1098@gmail.com>
2025-06-01 20:15:41 +0500

    implement imagekit

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+'use client';
 
-const ImageUpload = () => {
-  return (
-    <div>ImageUpload</div>
-  )
+import config from "@/lib/config";
+import { Image, ImageKitProvider, Upload, Video } from "@imagekit/next";
+
+const authenticator = async () => {
+  try {
+    const response = await fetch(`${config.env.apiEndpoint}/api/auth/imagekit`);
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(`Request failed with status ${response.status}: ${errorText}`);
+    }
+
+    const data = await response.json();
+    const { signature, token, expire } = data;
+    return {token, signature, expire};
+
+  } catch (error: any) {
+    throw new Error(`Authentication failed ${error.message}`);
+  }
 }
 
-export default ImageUpload
+const ImageUpload = () => {
+  return <div>ImageUpload</div>;
+};
+
+export default ImageUpload;
