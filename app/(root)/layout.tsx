688744c6917cfb48bdcfad2c5f525993c688122b
AbdullahSajid18 <abdullahsajid1098@gmail.com>
2025-06-09 15:01:46 +0500

    update

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,25 @@
 import { auth } from "@/auth";
 import Header from "@/components/Header";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import { eq } from "drizzle-orm";
 import { redirect } from "next/navigation";
+import { after } from "next/server";
 import React, { ReactNode } from "react";
 
 const Layout = async ({ children }: { children: ReactNode }) => {
   const session = await auth();
 
   if (!session) redirect("/sign-in");
+
+  after(async () => {
+    if (!session?.user?.id) return;
+
+    await db
+      .update(users)
+      .set({ lastActivityDate: new Date().toISOString().slice(0, 10) })
+      .where(eq(users.id, session?.user?.id));
+  });
 
   return (
     <main className="root-container">
