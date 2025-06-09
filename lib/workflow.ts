24561d4cc50d472bf6e8d67064489f9d05d2fb5f
AbdullahSajid18 <abdullahsajid1098@gmail.com>
2025-06-09 14:56:25 +0500

    implement resend

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,9 +1,35 @@
-import { Client as WorkflowClient} from '@upstash/workflow'
-import config from '@/lib/config'
-
-
+import { Client as WorkflowClient } from "@upstash/workflow";
+import { Client as QStashClient, resend } from "@upstash/qstash";
+import config from "@/lib/config";
 
 export const workflowClient = new WorkflowClient({
-    baseUrl: config.env.upstash.qstashUrl,
-    token: config.env.upstash.qstashToken,
-})
+  baseUrl: config.env.upstash.qstashUrl,
+  token: config.env.upstash.qstashToken,
+});
+
+export const sendEmail = async ({
+  email,
+  subject,
+  message,
+}: {
+  email: string;
+  subject: string;
+  message: string;
+}) => {
+  await qStashClient.publishJSON({
+    api: {
+      name: "email",
+      provider: resend({ token: config.env.resendToken }),
+    },
+    body: {
+      from: "Abdullah Sajid <hello.abdullahsajid.publicvm.com>",
+      to: [email],
+      subject: subject,
+      html: message,
+    },
+  });
+};
+
+const qStashClient = new QStashClient({
+  token: config.env.upstash.qstashToken,
+});
