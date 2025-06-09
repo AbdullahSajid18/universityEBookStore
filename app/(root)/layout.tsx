b70fb47011bddf1cd5347bbb9f4c8fd4378f6b14
AbdullahSajid18 <abdullahsajid1098@gmail.com>
2025-06-09 15:06:40 +0500

    update

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -15,6 +15,18 @@ const Layout = async ({ children }: { children: ReactNode }) => {
   after(async () => {
     if (!session?.user?.id) return;
 
+    // get the user and see if the last activity date is today
+    const user = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, session?.user?.id))
+      .limit(1);
+
+      if (user[0].lastActivityDate === new Date().toISOString().slice(0, 10)) {
+        return; 
+        
+      }
+
     await db
       .update(users)
       .set({ lastActivityDate: new Date().toISOString().slice(0, 10) })
