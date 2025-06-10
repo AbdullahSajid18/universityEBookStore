d32f06aa9757ba4e3af5874881cc4e6f08dd2b11
AbdullahSajid18 <abdullahsajid1098@gmail.com>
2025-06-10 16:24:04 +0500

    implement email sending and workflows

--- app/api/workflows/onboarding/route.ts
+++ app/api/workflows/onboarding/route.ts
@@ -1,15 +1,15 @@
-import { serve } from "@upstash/workflow/nextjs";
 import { db } from "@/database/drizzle";
 import { users } from "@/database/schema";
-import { eq } from "drizzle-orm";
 import { sendEmail } from "@/lib/workflow";
+import { serve } from "@upstash/workflow/nextjs";
+import { eq } from "drizzle-orm";
 
 type UserState = "non-active" | "active";
 
-type InitialData = {
+interface InitialData {
   email: string;
   fullName: string;
-};
+}
 
 const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000;
 const THREE_DAYS_IN_MS = 3 * ONE_DAY_IN_MS;
@@ -35,46 +35,48 @@ const getUserState = async (email: string): Promise<UserState> => {
     return "non-active";
   }
 
+  // Default return value for other cases
   return "active";
 };
 
 export const { POST } = serve<InitialData>(async (context) => {
   const { email, fullName } = context.requestPayload;
 
-  // Welcome Email
-  await context.run("new-signup", async () => {
+  // Step 1: Send welcome email
+  await context.run("new-signuphg", async () => {
     await sendEmail({
       email,
-      subject: "Welcome to the platform",
-      message: `Welcome ${fullName}!`,
+      subject: "Welcome to Our Service!",
+      message: `Welcom ${fullName}`,
     });
   });
 
-  await context.sleep("wait-for-3-days", 60 * 60 * 24 * 3);
+  // Step 2: Wait for 3 days (in seconds)
+  await context.sleep("sleep-until-follow-up", 60 * 60 * 24 * 3);
 
   while (true) {
-    const state = await context.run("check-user-state", async () => {
+    const state = await context.run('check-user-state', async () => {
       return await getUserState(email);
     });
 
-    if (state === "non-active") {
-      await context.run("send-email-non-active", async () => {
+    if (state === 'non-active') {
+      await context.run('send-email-non-active', async () => {
         await sendEmail({
           email,
-          subject: "Are you still there?",
-          message: `Hey ${fullName}, we miss you!`,
-        });
-      });
-    } else if (state === "active") {
-      await context.run("send-email-active", async () => {
+          subject: 'Are you still there?',
+          message: `Hey ${fullName}, we're missing you!`
+        })
+      })
+    } else if (state === 'active') {
+      await context.run('send-email-active', async () => {
         await sendEmail({
           email,
-          subject: "Welcome back!",
-          message: `Welcome back ${fullName}!`,
-        });
-      });
+          subject: 'Welcome back!',
+          message: `Welcome back ${fullName}`
+        })
+      })
     }
 
-    await context.sleep("wait-for-1-month", 60 * 60 * 24 * 30);
   }
-});
+
+})
