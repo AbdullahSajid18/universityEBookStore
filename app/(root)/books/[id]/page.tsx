e7caaa7f61420d991fd368c39b9a912d726b833f
AbdullahSajid18 <abdullahsajid1098@gmail.com>
2025-06-14 15:09:10 +0500

    implement full book-details section

--- app/(root)/books/[id]/page.tsx
+++ app/(root)/books/[id]/page.tsx
@@ -1,9 +1,11 @@
 import { auth } from "@/auth";
+import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
 import BookVideo from "@/components/BookVideo";
 import { db } from "@/database/drizzle";
 import { books } from "@/database/schema";
-import { eq } from "drizzle-orm";
+import { BookProps } from "@/types";
+import { desc, eq, ne } from "drizzle-orm";
 import { redirect } from "next/navigation";
 
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
@@ -19,11 +21,17 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
   if (!bookDetails) redirect("/404");
 
+  const similarBooks = (await db
+    .select()
+    .from(books)
+    .where(ne(books.id, bookDetails.id)) // ✅ Exclude current book
+    .orderBy(desc(books.createdAt))
+    .limit(10)) as BookProps[];
   return (
     <>
       <BookOverview {...bookDetails} userId={session?.user?.id as string} />
 
-      <div className="book-details">
+      <div className="book-details flex flex-col">
         <div className="flex-[1.5]">
           <section className="flex flex-col gap-7">
             <h3>Video</h3>
@@ -32,16 +40,18 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
           <section className="mt-10 flex flex-col gap-7">
             <h3>Summary</h3>
             <div className="space-y-5 text-xl text-light-100">
-                {bookDetails.summary.split('\n').map((line, index) => (
-                    <p key={index}>{line}</p>
-                ))}
-
+              {bookDetails.summary.split("\n").map((line, index) => (
+                <p key={index}>{line}</p>
+              ))}
             </div>
-
           </section>
         </div>
 
-        {/* Similar Books  */}
+        <BookList
+          title="Similar Books"
+          books={similarBooks}
+          containerClassName="mt-28"
+        />
       </div>
     </>
   );
