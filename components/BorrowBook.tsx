c58bafb4c4f240794b7c693179bb1dc7523878a2
AbdullahSajid18 <abdullahsajid1098@gmail.com>
2025-06-17 23:21:42 +0500

    implement some changes in borrow book component

--- components/BorrowBook.tsx
+++ components/BorrowBook.tsx
@@ -1,11 +1,41 @@
-'use client';
+"use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { BorrowingBookProps } from "@/types";
+import { useRouter } from "next/navigation";
+import { toast } from "@/hooks/use-toast";
 
-const BorrowBook = ({bookId, userId, borrowingEligibility}: BorrowingBookProps) => {
+const BorrowBook = ({
+  bookId,
+  userId,
+  borrowingEligibility: { isEligible, message },
+}: BorrowingBookProps) => {
+  const router = useRouter();
+  const [borrowing, setBorrowing] = useState(false);
+
+  const borrowHandler = async () => {
+    if (!isEligible) {
+      toast({
+        title: "Error",
+        description: message,
+        variant: "destructive",
+      });
+    }
+
+    try {
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: "An error occured while borrowing the book",
+        variant: "destructive",
+      });
+    } finally {
+      setBorrowing(false);
+    }
+  };
+
   return (
     <Button className="book-overview_btn">
       <Image src="/icons/book.svg" alt="book" width={20} height={20} />
